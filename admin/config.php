e62c541c9ae4fddf35124f9224123c64d6a5236a
Himanshu <himanshu.208@gmail.com>
2015-11-05 21:56:38 +0530

    New Setup FOr car project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/carproject/admin/');
+define('HTTP_CATALOG', 'http://localhost/carproject/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/carproject/admin/');
+define('HTTPS_CATALOG', 'http://localhost/carproject/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/xampp/htdocs/carproject/admin/');
+define('DIR_SYSTEM', 'F:/xampp/htdocs/carproject/system/');
+define('DIR_LANGUAGE', 'F:/xampp/htdocs/carproject/admin/language/');
+define('DIR_TEMPLATE', 'F:/xampp/htdocs/carproject/admin/view/template/');
+define('DIR_CONFIG', 'F:/xampp/htdocs/carproject/system/config/');
+define('DIR_IMAGE', 'F:/xampp/htdocs/carproject/image/');
+define('DIR_CACHE', 'F:/xampp/htdocs/carproject/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/xampp/htdocs/carproject/system/storage/download/');
+define('DIR_LOGS', 'F:/xampp/htdocs/carproject/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/xampp/htdocs/carproject/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/xampp/htdocs/carproject/system/storage/upload/');
+define('DIR_CATALOG', 'F:/xampp/htdocs/carproject/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'carproject');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'car_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/carproject/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/carproject/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/xampp/htdocs/carproject/catalog/');
+define('DIR_SYSTEM', 'F:/xampp/htdocs/carproject/system/');
+define('DIR_LANGUAGE', 'F:/xampp/htdocs/carproject/catalog/language/');
+define('DIR_TEMPLATE', 'F:/xampp/htdocs/carproject/catalog/view/theme/');
+define('DIR_CONFIG', 'F:/xampp/htdocs/carproject/system/config/');
+define('DIR_IMAGE', 'F:/xampp/htdocs/carproject/image/');
+define('DIR_CACHE', 'F:/xampp/htdocs/carproject/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/xampp/htdocs/carproject/system/storage/download/');
+define('DIR_LOGS', 'F:/xampp/htdocs/carproject/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/xampp/htdocs/carproject/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/xampp/htdocs/carproject/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'carproject');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'car_');
